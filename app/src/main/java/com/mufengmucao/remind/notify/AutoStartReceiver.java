13afa1692301a5e4ff8a801ece71904142898d30
lith <lith@guahao.com>
2024-06-28 17:18:25 +0800

    开机启动

--- app/src/main/java/com/mufengmucao/remind/notify/AutoStartReceiver.java
+++ app/src/main/java/com/mufengmucao/remind/notify/AutoStartReceiver.java
@@ -4,6 +4,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import com.mufengmucao.remind.MainActivity;
+import com.mufengmucao.remind.tile.MedicalRemindTileService;
 
 /**
  * 开机启动监听事件，用于自启动服务
@@ -14,12 +15,12 @@ public class AutoStartReceiver extends BroadcastReceiver {
         if (Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction())) {
             // 应用程序在开机后执行的逻辑
             // 例如启动服务或者Activity
-//            Intent serviceIntent = new Intent(context, YourService.class);
-//            context.startService(serviceIntent);
+            Intent serviceIntent = new Intent(context, MedicalRemindTileService.class);
+            context.startService(serviceIntent);
             // 或者
-             Intent activityIntent = new Intent(context, MainActivity.class);
-             activityIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-             context.startActivity(activityIntent);
+            Intent activityIntent = new Intent(context, MainActivity.class);
+            activityIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            context.startActivity(activityIntent);
         }
     }
 }
